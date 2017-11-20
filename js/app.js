2f17190a7e5df9d6b76c7aa5ee7f6211a65c0d2d
AlejandraCP <alejandracp710@gmail.com>
2017-11-20 05:05:15 -0500

    Mejorando indentación y agregando comentarios

--- js/app.js
+++ js/app.js
@@ -1,72 +1,75 @@
 var box = document.getElementById('container');
 var tarea = document.getElementById('span');
 
-window.onload = function () {
-    add();
-}
+window.onload = function() {
+  add();
+};
 
 function add() {
-    tarea.addEventListener('click', function () {
-                var input = document.createElement('input');
-                var btn = document.createElement('button');
-                var icon = document.createElement('i');
-                var div = document.createElement('div');
 
-                btn.textContent = 'Guardar';
-                icon.setAttribute('class', 'fa fa-times');
-                div.appendChild(input);
-                div.appendChild(btn);
-                div.appendChild(icon);
+  // función que crea un input y le botón
+  tarea.addEventListener('click', function() {
+    var input = document.createElement('input');
+    var btn = document.createElement('button');
+    var icon = document.createElement('i');
+    var div = document.createElement('div');
 
-                btn.classList.add('button');
-                div.setAttribute('class', 'style-box');
+    btn.textContent = 'Guardar';
+    icon.setAttribute('class', 'fa fa-times');
+    div.appendChild(input);
+    div.appendChild(btn);
+    div.appendChild(icon);
 
-                box.appendChild(div);
-                box.removeChild(tarea);
+    btn.classList.add('button');
+    div.setAttribute('class', 'style-box');
 
-                btn.addEventListener('click', function () {
+    box.appendChild(div);
+    box.removeChild(tarea);
 
-                    var div2 = document.createElement('div');
-                    var tarea2 = document.createElement('div');
-                    var text = document.createElement('p');
-                    var tareaText = document.createElement('span');
+    // función que crea un div con el contenido del input mas un div con el contenido 'Añadir tarea'
+    btn.addEventListener('click', function() {
+      var div2 = document.createElement('div');
+      var tarea2 = document.createElement('div');
+      var text = document.createElement('p');
+      var tareaText = document.createElement('span');
 
-                    div2.classList.add('left');
-                    text.textContent = input.value;
-                    input.value = '';
-                    div2.appendChild(text);
-                    tareaText.textContent = 'Añadir tarea';
-                    tarea2.appendChild(tareaText);
-                    div2.appendChild(tarea2);
-                    box.insertBefore(div2, div);
+      div2.classList.add('left');
+      text.textContent = input.value;
+      input.value = '';
+      div2.appendChild(text);
+      tareaText.textContent = 'Añadir tarea';
+      tarea2.appendChild(tareaText);
+      div2.appendChild(tarea2);
+      box.insertBefore(div2, div);
 
-                    div2.setAttribute('class', 'style-box');
-                    tarea2.setAttribute('class','shadow');
+      div2.setAttribute('class', 'style-box');
+      tarea2.setAttribute('class', 'shadow');
 
+      // función al hacer clíck en Añadir tarea se crea un input y un botón
+      tareaText.addEventListener('click', function() {
+        var input2 = document.createElement('input');
+        var span2 = document.createElement('i');
+        var button2 = document.createElement('button');
 
-                    tareaText.addEventListener('click', function () {
-                    var input2 = document.createElement('input');
-                    var span2 = document.createElement('i');
-                    var button2 = document.createElement('button');
+        span2.setAttribute('class', 'fa fa-times');
+        button2.textContent = 'Añadir';
 
-                    span2.setAttribute('class', 'fa fa-times');
-                    button2.textContent = 'Añadir';
+        button2.classList.add('button');
 
-                    tarea2.appendChild(input2);
-                    tarea2.appendChild(button2);
-                    tarea2.appendChild(span2);
-                    tarea2.removeChild(tareaText);
+        tarea2.appendChild(input2);
+        tarea2.appendChild(button2);
+        tarea2.appendChild(span2);
+        tarea2.removeChild(tareaText);
 
-                    button2.addEventListener('click', function () {
+        // función crea un párrafo con el contenido del segundo input
+        button2.addEventListener('click', function() {
 
-                        var parr = document.createElement('p');
-                        parr.textContent = input2.value;
-                        input2.value = ' ';
-                        div2.insertBefore(parr, tarea2);
-                    })
-                })
-            })
-
-    })
+          var parr = document.createElement('p');
+          parr.textContent = input2.value;
+          input2.value = ' ';
+          div2.insertBefore(parr, tarea2);
+        });
+      });
+    });
+  });
 }
-
